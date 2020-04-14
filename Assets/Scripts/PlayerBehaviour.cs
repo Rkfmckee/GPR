feb81521aff392f093dffbb63ab0fc6e9abdf6a1
Ryan McKeever <rkfmckee@gmail.com>
2020-04-14 17:49:03 +0100

    Add seePlayerThroughWalls into it's own script, and make the names generic for any object

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -14,21 +14,12 @@ public class PlayerBehaviour : MonoBehaviour
     private float zSpeed;
     private float xSpeed;
 
-    private GameObject currentObjectHit;
-    private GameObject previousObjectHit;
-    private GameObject nextObjectHit;
-
-
     #endregion
 
     #region Events
 
     private void Awake() {
         setupInstanceVariables();
-
-        currentObjectHit = null;
-        previousObjectHit = null;
-        nextObjectHit = null;
     }
 
     private void Start() {
@@ -36,7 +27,6 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Update() {
         calculateMovement();
-        seePlayerThroughWalls();
     }
 
     #endregion
@@ -55,40 +45,6 @@ public class PlayerBehaviour : MonoBehaviour
 
         rigidbody.velocity = movementAmount;
         transform.LookAt(newPosition);
-    }
-
-    private void seePlayerThroughWalls() {
-        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
-        RaycastHit hit;
-        Vector3 fromCameraToPlayer = -(camera.transform.position - transform.position);
-        Material solidMaterial = Resources.Load("Materials/WallMaterial") as Material;
-        Material translucentMaterial = Resources.Load("Materials/WallMaterialTranslucent") as Material;
-
-        if (Physics.Raycast(camera.transform.position, fromCameraToPlayer, out hit)) {
-            Debug.DrawRay(camera.transform.position, fromCameraToPlayer * hit.distance, Color.yellow);
-
-            nextObjectHit = hit.collider.gameObject;
-
-            if (nextObjectHit != currentObjectHit) {
-                previousObjectHit = currentObjectHit;
-                currentObjectHit = nextObjectHit;
-
-                if (previousObjectHit != null) {
-                    if (previousObjectHit.tag == "Wall") {
-                        previousObjectHit.GetComponent<Renderer>().material = solidMaterial;
-                    }
-                }
-
-                if (currentObjectHit != null) {
-                    if (currentObjectHit.tag == "Wall") {
-                        currentObjectHit.GetComponent<Renderer>().material = translucentMaterial;
-                    }
-                }
-            }
-
-        } else {
-            Debug.DrawRay(camera.transform.position, fromCameraToPlayer * 1000, Color.white);
-        }
     }
 
     #endregion
