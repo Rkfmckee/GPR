e31bb550dfc11fd08c003fb04c9be285814a2b97
Ryan McKeever <rkfmckee@gmail.com>
2020-07-07 18:50:11 +0100

    Update Camera movement to be more smooth

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -12,6 +13,7 @@ public class CameraController : MonoBehaviour
     private GameObject currentPlayer;
 
     private float cameraDifferenceToPlayerZPosition;
+    private States currentState;
 
     #region Events
 
@@ -26,8 +28,10 @@ public class CameraController : MonoBehaviour
         cameraDifferenceToPlayerZPosition = currentPlayer.transform.position.z - camera.transform.position.z;
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        // Since the player movement is in FixedUpdate, putting this in the same event makes a smoother movement.
+
         Vector3 newCameraPosition = new Vector3(currentPlayer.transform.position.x, camera.transform.position.y, currentPlayer.transform.position.z - cameraDifferenceToPlayerZPosition);
 
         camera.transform.position = ChangePosition(newCameraPosition);
@@ -39,30 +43,47 @@ public class CameraController : MonoBehaviour
 
     public void SetCurrentlyControlledPlayer(GameObject player) {
         currentPlayer = player;
+        currentState = States.Transitioning;
     }
 
     private void FindStartingPlayer() {
         foreach (var currentPlayer in allPlayers) {
             if (currentPlayer.GetComponent<PlayerBehaviour>().currentlyBeingControlled) {
                 this.currentPlayer = currentPlayer;
+                currentState = States.Following;
                 return;
             }
         }
     }
 
     private Vector3 ChangePosition(Vector3 newPosition) {
-        // Only use smooth transition if the distance is greater than 0.1
+        // Only use smooth transition if in the Transitioning state
+
+        float distanceUntilEndTransition = 0.1f;
 
         float transitionTime = 0;
         float xDifference = camera.transform.position.x - newPosition.x;
         float zDifference = camera.transform.position.z - newPosition.z;
         Vector3 differenceToNewPosition = new Vector3(xDifference, 0, zDifference);
 
-        if (differenceToNewPosition.magnitude > 0.5) {
-            transitionTime = cameraTransitionTime;
+        if (currentState == States.Transitioning) {
+            if (differenceToNewPosition.magnitude > distanceUntilEndTransition) {
+                transitionTime = cameraTransitionTime;
+            } else {
+                currentState = States.Following;
+            }
         }
 
         return Vector3.SmoothDamp(camera.transform.position, newPosition, ref velocity, transitionTime);
+    }
+
+    #endregion
+
+    #region Enums
+
+    public enum States {
+        Transitioning,
+        Following
     }
 
     #endregion
