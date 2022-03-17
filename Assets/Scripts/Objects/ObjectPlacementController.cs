8f35206091ab57478afdc91ad11aea72ed462f4b
Ryan McKeever <rkfmckee@gmail.com>
2022-03-17 15:51:49 +0000

    Check for terrain mask and valid placement

--- Assets/Scripts/Objects/ObjectPlacementController.cs
+++ Assets/Scripts/Objects/ObjectPlacementController.cs
@@ -4,51 +4,48 @@ using UnityEngine;
 public class ObjectPlacementController : MonoBehaviour {
 	#region Properties
 
+	[HideInInspector]
 	public bool validPlacement;
+	[HideInInspector]
 	public RaycastHit hitInformation;
+	public float maxPlacementDistance;
 
-	private GameObject placementObject;
-	private MeshRenderer placementObjectRenderer;
 	private Material validMaterial;
 	private Material invalidMaterial;
 	private Dictionary<string, string> placementPrefabs;
+	private int floorMask;
+	private int wallMask;
+	private int terrainMask;
+
+	private new Camera camera;
+	private GameObject placementObject;
+	private MeshRenderer placementObjectRenderer;
 	private new MeshRenderer renderer;
-	private MeshFilter meshFilter;
-	private Mesh heldObjectMesh;
 
 	#endregion
 
 	#region Events
 
 	private void Awake() {
+		camera = Camera.main;
 		renderer = GetComponent<MeshRenderer>();
-		meshFilter = GetComponent<MeshFilter>();
-
-		validPlacement = true;
 
 		validMaterial = Resources.Load("Materials/Objects/Placement/ValidPlacement") as Material;
 		invalidMaterial = Resources.Load("Materials/Objects/Placement/InvalidPlacement") as Material;
-
 		renderer.material = validMaterial;
+
+		validPlacement = true;
+
+		floorMask = 1 << LayerMask.NameToLayer("Floor");
+        wallMask = 1 << LayerMask.NameToLayer("Wall");
+        terrainMask = floorMask | wallMask;
 
 		SetPlacementPrefabs();
 	}
 
 	private void Update() {
-		Ray cameraToMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-		if (Physics.Raycast(cameraToMouse, out hitInformation, Mathf.Infinity)) {
-			transform.position = hitInformation.point;
-		}
-
-		if (placementObjectRenderer != null) {
-			if (validPlacement) {
-				placementObjectRenderer.material = validMaterial;
-			} else {
-				placementObjectRenderer.material = invalidMaterial;
-			}
-		}
-			
+		CheckForRaycastHit();
+		ValidPlacementChangeMaterial();
 	}
 
 	#endregion
@@ -77,6 +74,32 @@ public class ObjectPlacementController : MonoBehaviour {
 		placementPrefabs = new Dictionary<string, string> {
 			{"Crate", "CratePlacement"}
 		};
+	}
+
+	private void CheckForRaycastHit() {
+        var cameraToMouseRay = camera.ScreenPointToRay(Input.mousePosition);
+        validPlacement = true;
+
+        if (Physics.Raycast(cameraToMouseRay, out hitInformation, Mathf.Infinity, terrainMask)) {
+            Vector3 pointHit = hitInformation.point;
+
+            validPlacement = GetValidDistance(pointHit);
+            transform.position = pointHit;
+        }
+	}
+
+    private bool GetValidDistance(Vector3 pointHit) {
+        return Vector3.Distance(References.Player.currentPlayer.transform.position, pointHit) <= maxPlacementDistance;
+    }
+
+	private void ValidPlacementChangeMaterial() {
+		if (placementObjectRenderer != null) {
+			if (validPlacement) {
+				placementObjectRenderer.material = validMaterial;
+			} else {
+				placementObjectRenderer.material = invalidMaterial;
+			}
+		}
 	}
 
 	#endregion
