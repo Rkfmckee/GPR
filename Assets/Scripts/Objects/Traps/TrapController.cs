7134a4f2b87dfce2418aff59ef9cca1227ac7366
Ryan McKeever <rkfmckee@gmail.com>
2022-03-17 15:51:15 +0000

    Reorganise files some more

---
+++ Assets/Scripts/Objects/Traps/TrapController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.MemoryProfiler;
+using UnityEngine;
+
+public abstract class TrapController : MonoBehaviour {
+    #region Properties
+
+    protected Type trapType;
+
+    #endregion
+
+    #region Methods
+
+    public abstract void TriggerTrap(Collider triggeredBy);
+
+    public Type GetTrapType() { return trapType; }
+
+    #endregion
+
+    #region Enums
+
+    public enum Type {
+        Floor,
+        Wall
+    }
+
+    #endregion
+}
