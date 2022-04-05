b7e8962d44e8017fc4922f190b34e590513d2d8a
Ryan McKeever <rkfmckee@gmail.com>
2022-04-06 00:13:09 +0100

    Add camera boundaries, and scroll movement

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -7,7 +7,11 @@ public class CameraController : MonoBehaviour {
 	public float cameraTransitionTime;
 	public float movementSpeed;
 	public float orientationChangeTime;
+	[VectorLabels("Minimum", "Maximum")]
+	public Vector2 yBounds;
 
+	private Vector2 xBounds;
+	private Vector2 zBounds;
 	private new Camera camera;
 	private Vector3 velocity;
 	private CameraState currentState;
@@ -37,25 +41,62 @@ public class CameraController : MonoBehaviour {
 
 		currentState = CameraState.CONTROLLED_MOVEMENT;
 		currentOrientation = CameraOrientation.ANGLED;
+
+		UpdateFloorObjectBoundaries();
 	}
 
 	void Update() {
-		if (Input.GetKeyDown("space")) {
-			ChangeOrientation();
-		}
+		if (currentState == CameraState.TRANSITIONING)
+		return;
 
-		if (currentState == CameraState.CONTROLLED_MOVEMENT) {
-			HandleMovement();
+		if (Input.GetButtonDown("Jump")) {
+			ChangeOrientation();
 		}
 
 		if (currentOrientation == CameraOrientation.ANGLED) {
 			SeeThroughWalls();
 		}
+
+		HandleMovement();
 	}
 
 	#endregion
 
 	#region Methods
+
+	public void UpdateFloorObjectBoundaries() {
+		GameObject[] allFloorObjects = GameObject.FindGameObjectsWithTag("Floor");
+		xBounds = new Vector2();
+		zBounds = new Vector2();
+		float minX, maxX, minZ, maxZ;
+
+		if (allFloorObjects.Length == 0)
+			return;
+		
+		minX = maxX = allFloorObjects[0].transform.position.x;
+		minZ = maxZ = allFloorObjects[0].transform.position.z;
+
+		foreach(var floor in allFloorObjects) {
+			var position = floor.transform.position;
+
+			if (position.x < minX)
+				minX = position.x;
+			
+			if (position.x > maxX)
+				maxX = position.x;
+
+			if (position.z < minZ)
+				minZ = position.z;
+			
+			if (position.z > maxZ)
+				maxZ = position.z;
+		}
+
+		xBounds.x = minX;
+		xBounds.y = maxX;
+		zBounds.x = minZ;
+		zBounds.y = maxZ;
+	}
 
 	private void HandleMovement() {
 		float verticalAxis = Input.GetAxis("Vertical");
@@ -67,41 +108,52 @@ public class CameraController : MonoBehaviour {
 		}
 
 		direction = NormaliseVectorToKeepDeceleration(direction);
+		direction += AddScrollMovement();
+
 		Vector3 movementAmount = direction * movementSpeed * Time.deltaTime;
 		transform.Translate(movementAmount);
+		ClampPositionBoundaries();
+	}
+
+	private Vector3 AddScrollMovement() {
+		if (currentOrientation != CameraOrientation.TOP_DOWN)
+			return Vector3.zero;
+		
+		float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
+		return Vector3.forward * scrollAxis * 50;
+	}
+
+	private void ClampPositionBoundaries() {
+		// Positions stored as Vector2's
+		// X = minimum, Y = maximum
+		
+		Vector3 clampedPosition = transform.position;
+		float zOffset = 0;
+
+		if (currentOrientation == CameraOrientation.TOP_DOWN) {
+			float clampedY = Mathf.Clamp(clampedPosition.y, yBounds.x, yBounds.y);
+			clampedPosition.y = clampedY;
+		} else {
+			zOffset = -6;
+		}
+
+		float clampedX = Mathf.Clamp(clampedPosition.x, xBounds.x, xBounds.y);
+		clampedPosition.x = clampedX;
+		float clampedZ = Mathf.Clamp(clampedPosition.z, zBounds.x + zOffset, zBounds.y + zOffset);
+		clampedPosition.z = clampedZ;
+
+		transform.position = clampedPosition;
 	}
 
 	private void ChangeOrientation() {
-		print("Change orientation");
 		CameraOrientation orientation = CameraOrientation.TOP_DOWN;
 
 		if (currentOrientation == CameraOrientation.TOP_DOWN) {
 			orientation = CameraOrientation.ANGLED;
 		}
-		
+
 		StartCoroutine(TransitionOrientation(orientation));
 	}
-
-	// private Vector3 ChangePosition(Vector3 newPosition) {
-	// 	// Only use smooth transition if in the Transitioning state
-
-	// 	float distanceUntilEndTransition = 0.1f;
-
-	// 	float transitionTime = 0;
-	// 	float xDifference = camera.transform.position.x - newPosition.x;
-	// 	float zDifference = camera.transform.position.z - newPosition.z;
-	// 	Vector3 differenceToNewPosition = new Vector3(xDifference, 0, zDifference);
-
-	// 	if (currentState == CameraState.TRANSITIONING) {
-	// 		if (differenceToNewPosition.magnitude > distanceUntilEndTransition) {
-	// 			transitionTime = cameraTransitionTime;
-	// 		} else {
-	// 			currentState = CameraState.CONTROLLED_MOVEMENT;
-	// 		}
-	// 	}
-
-	// 	return Vector3.SmoothDamp(camera.transform.position, newPosition, ref velocity, transitionTime);
-	// }
 
 	private void SeeThroughWalls() {
 		Vector3 direction = transform.forward;
@@ -134,17 +186,49 @@ public class CameraController : MonoBehaviour {
 			}
 
 			if (!isHit) {
-				GameObject wasHidden = hiddenObjects[i];
-				wasHidden.GetComponent<Renderer>().enabled = true;
-
-				if (wasHidden.layer == LayerMask.NameToLayer("WallIgnoreRaycast")) {
-					wasHidden.layer = LayerMask.NameToLayer("Wall");
-				}
+				ClearHiddenObject(hiddenObjects[i]);
 
 				hiddenObjects.RemoveAt(i);
 				i--;
 			}
 		}
+	}
+
+	private void ClearAllHiddenObjects() {
+		foreach(var hiddenObject in hiddenObjects) {
+			ClearHiddenObject(hiddenObject);
+		}
+
+		hiddenObjects.Clear();
+	}
+
+	private void ClearHiddenObject(GameObject hiddenObject) {
+		hiddenObject.GetComponent<Renderer>().enabled = true;
+
+		if (hiddenObject.layer == LayerMask.NameToLayer("WallIgnoreRaycast")) {
+			hiddenObject.layer = LayerMask.NameToLayer("Wall");
+		}
+	}
+
+	private Vector3 ChangePosition(Vector3 newPosition) {
+		// Only use smooth transition if in the Transitioning state
+
+		float distanceUntilEndTransition = 0.1f;
+
+		float transitionTime = 0;
+		float xDifference = camera.transform.position.x - newPosition.x;
+		float zDifference = camera.transform.position.z - newPosition.z;
+		Vector3 differenceToNewPosition = new Vector3(xDifference, 0, zDifference);
+
+		if (currentState == CameraState.TRANSITIONING) {
+			if (differenceToNewPosition.magnitude > distanceUntilEndTransition) {
+				transitionTime = cameraTransitionTime;
+			} else {
+				currentState = CameraState.CONTROLLED_MOVEMENT;
+			}
+		}
+
+		return Vector3.SmoothDamp(camera.transform.position, newPosition, ref velocity, transitionTime);
 	}
 
 	#endregion
@@ -175,6 +259,10 @@ public class CameraController : MonoBehaviour {
 			orientationChangeTimeCurrent += Time.deltaTime;
 
 			yield return null;
+		}
+
+		if (orientation == CameraOrientation.TOP_DOWN) {
+			ClearAllHiddenObjects();
 		}
 
 		currentOrientation = orientation;
