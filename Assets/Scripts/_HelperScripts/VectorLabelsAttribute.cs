f342cf500e434adfe702c920f15982f42b1a4859
Ryan McKeever <rkfmckee@gmail.com>
2022-04-06 00:12:36 +0100

    Add Editor script for adding label to Vectors in inspector

---
+++ Assets/Scripts/_HelperScripts/VectorLabelsAttribute.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+//
+//  VectorLabelsAttribute.cs
+//  VectorLabels
+//
+//  Created by Hellium on 21/11/18.
+//  https://answers.unity.com/questions/1573537/how-to-change-the-names-of-a-vector-3-that-is-set.html
+//
+
+public class VectorLabelsAttribute : PropertyAttribute {
+	public readonly string[] Labels;
+
+	public VectorLabelsAttribute(params string[] labels) {
+		Labels = labels;
+	}
+}
